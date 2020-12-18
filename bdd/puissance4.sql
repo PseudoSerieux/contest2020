f0091f79515b35263527974c58cdefb6294d4dd1
YannLBC <lebouc.yann@hotmail.com>
2020-12-18 16:39:24 +0100

    BDD

--- bdd/puissance4.sql
+++ bdd/puissance4.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.3
+-- version 4.8.5
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  ven. 18 déc. 2020 à 12:43
--- Version du serveur :  5.7.23
--- Version de PHP :  7.4.7
+-- Généré le :  ven. 18 déc. 2020 à 15:38
+-- Version du serveur :  5.7.26
+-- Version de PHP :  7.2.18
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -34,15 +34,19 @@ CREATE TABLE IF NOT EXISTS `membres` (
   `nom` varchar(25) NOT NULL,
   `mdp` char(32) NOT NULL,
   `mail` varchar(255) NOT NULL,
+  `score` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `membres`
 --
 
-INSERT INTO `membres` (`id`, `nom`, `mdp`, `mail`) VALUES
-(1, 'nom', 'mdp', 'mail@mail.com');
+INSERT INTO `membres` (`id`, `nom`, `mdp`, `mail`, `score`) VALUES
+(1, 'patric', 'test1234', 'mail@mail.com', 5),
+(2, 'Lola', 'test1234', 'mail@mail.com', 3),
+(3, 'Patricdu44', 'test1234', 'mail@mail.com', 2),
+(4, 'matttt', 'test1234', 'mail@mail.com', 4);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
