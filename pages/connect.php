f8a68bf83697dba61fc05e819a72f09b36fc4930
Margaux <margaux.dechaud@imie.fr>
2020-12-18 10:55:09 +0100

    Gestion inscription + page index = page connexion

---
+++ pages/connect.php
@@ -0,0 +1,28 @@
+<?php
+
+session_start();
+
+$bdd = new PDO(
+    'mysql:host=localhost; dbname=puissance4', 'root', '',);
+
+function printBeautify($text)
+{
+
+    echo $text . "\n";
+
+}
+
+function formatErrorMsg($errors, $icon = "&#9888;")
+{
+
+    $return = "";
+    foreach ($errors as $key => $msg) {
+        //AFFICHE les éléments "$msg"
+        $return.='<p class="msg_error">' . $icon . ' ' . upper($msg) . '</p>';
+    }
+    return $return;
+}
+
+function upper($str){
+    return mb_strtoupper($str, 'UTF8');
+}
